825970cf46a9ad3d24fa8fb5dbe6d7094dcecda1
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-09 17:21:50 +0530

    navbar dropdown

--- src/Sheared/Navbar.js
+++ src/Sheared/Navbar.js
@@ -7,25 +7,29 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-base-100 bg-accent text-white">
   <div className="navbar-start">
-    {/* <div className="dropdown">
+    <div className="dropdown">
       <label tabIndex="0" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <svg xmlns="" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+      <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52  bg-accent text-white">
+        <li><a>Home</a></li>
         <li tabIndex="0">
           <a className="justify-between">
             Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+            <svg className="fill-current" xmlns="" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
           </a>
           <ul className="p-2">
             <li><a>Submenu 1</a></li>
             <li><a>Submenu 2</a></li>
           </ul>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>purchase</a></li>
+        <li><a>Business Summery</a></li>
+        <li><a>Reviews</a></li>
+        <li><a>Contact Us</a></li>
+        <li><a>Login</a></li>
       </ul>
-    </div> */}
+    </div>
     <Link to='/' className="btn btn-ghost normal-case text-xl"><img src={logo} alt='images'></img></Link>
   </div>
   <div className="navbar-center hidden lg:flex">
