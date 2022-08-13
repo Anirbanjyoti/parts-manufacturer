77fc86d405a16b570aff6d78b216b4ae631fc774
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-13 21:09:21 +0530

    load my order data if admin load all order in dash board

--- src/Pages/Dashboard/User/MyOrder.js
+++ src/Pages/Dashboard/User/MyOrder.js
@@ -1,44 +1,78 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 
 const MyOrder = () => {
   const [user] = useAuthState(auth);
-  const [shipping, setShipping] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
-    const url = "http://localhost:5000/shippingDetails";
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setShipping(data));
-  }, []);
+    if (user) {
+      fetch(`http://localhost:5000/order?email=${user.email}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+        .then((res) => {
+          console.log("res", res);
+          if (res.status === 401 || res.status === 403) {
+            signOut(auth);
+            localStorage.removeItem("accessToken");
+            navigate("/");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setOrders(data);
+        });
+    }
+  }, [user, navigate]);
   return (
     <div className="overflow-x-auto">
       <table className="table w-full text-left">
         <thead>
           <tr>
             {/* <th className=""></th> */}
-            <th>Name</th>
+            <th>SL</th>
             <th>Product ID</th>
+            <th>Email</th>
             <th>Address</th>
             <th>Phone</th>
             <th>Shipping</th>
           </tr>
         </thead>
         <tbody>
-          {/* {shipping?.map((ship) => (
-            <>
-              <tr>
-                <td>{user.displayName}</td>
-                <td>{ship._id}</td>
-                <td>{ship.address}</td>
-                <td>{ship.phone}</td>
-                <td>
-                  <button>pending...</button>
-                </td>
-              </tr>
-            </>
-          ))} */}
-
+          {orders.map((order, index) => (
+            <tr key={order._id}>
+              <th>{index + 1}</th>
+              <th>{order._id}</th>
+              <td>{order.email}</td>
+              <td>{order.address}</td>
+              <td>{order.phone}</td>
+             
+              <td>
+                {/* {order.price && !order.paid && (
+                  <Link to={`/dashboard/payment/${order._id}`}>
+                    <button className="btn btn-xs btn-success">pay</button>
+                  </Link>
+                )}
+                {order.price && order.paid && (
+                  <div>
+                    <p>
+                      <span className="text-success">Paid</span>
+                    </p>
+                    <p>
+                      Transaction id:{" "}
+                      <span className="text-success">{order.transactionId}</span>
+                    </p>
+                  </div>
+                )} */}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
