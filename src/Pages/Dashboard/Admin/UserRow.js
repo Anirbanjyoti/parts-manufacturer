b85851b59090b4629c070779aed45341ecd00ceb
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-13 11:16:08 +0530

    MAke admin Function and show user dashboard Dynamic

--- src/Pages/Dashboard/Admin/UserRow.js
+++ src/Pages/Dashboard/Admin/UserRow.js
@@ -1,42 +1,41 @@
 import React from "react";
-// import { toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const UserRow = ({ user, refetch }) => {
-  // const { _id, email, role } = user;
+  const { _id, email, role } = user;
   const makeAdmin = () => {
-    // fetch(`http://localhost:5000/user/admin/${email}`, {
-    //   method: "PUT",
-    //   headers: {
-    //     authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-    //   },
-    // })
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
       // .then((res) => res.json())
-      // .then((res) =>
-      // {
-      //   if (res.status === 403) {
-      //     toast.error("Failed to make an admin");
-      //   }
-      //   return res.json();
-      // })
-      // .then((data) => {
-      //   if (data.modifiedCount > 0) {
-      //     refetch();
-      //     toast("Successfully Made admin");
-      //   }
-      // });
+      .then((res) =>
+      {
+        if (res.status === 403) {
+          toast.error("Failed to make an admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast("Successfully Made admin");
+        }
+      });
   };
   return (
     <tr>
-      <th>ID</th>
-      <td>Email</td>
+      <th>{_id}</th>
+      <td>{email}</td>
       <td>
-        {/* {role !== "admin" && (
+        {role !== "admin" && (
           <button onClick={makeAdmin} className="btn btn-xs">
             Make Admin
           </button>
-        )} */}
-        <button>Make Admin</button>
+        )}
       </td>
       <td>
         <button className="btn btn-xs">Remove User</button>
--- src/Pages/Dashboard/Admin/Users.js
+++ src/Pages/Dashboard/Admin/Users.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../../Sheared/Loading";
-// import UserRow from "./UserRow";
-
+import UserRow from "./UserRow";
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery(["user"], () =>
+  const { data: users, isLoading, refetch } = useQuery(["user"], () =>
     fetch("http://localhost:5000/user", {
       method: "GET",
       headers: {
@@ -19,7 +18,7 @@ const Users = () => {
   }
   return (
     <div>
-      <h1>Total users:{users.length} </h1>
+      <h1>Total users: {users.length}</h1>
 
       <div className="overflow-x-auto">
         <table className="table table-zebra w-full ">
@@ -32,19 +31,9 @@ const Users = () => {
             </tr>
           </thead>
           <tbody>
-            {/* {users?.map((user) => (
+            {users.map((user) => (
               <UserRow key={user._id} user={user} refetch={refetch}></UserRow>
-            ))} */}
-            <tr>
-      <th>1</th>
-      <td>eerer</td>
-      <td>
-        <button>Make Admin</button>
-      </td>
-      <td>
-        <button className="btn btn-xs">Remove User</button>
-      </td>
-    </tr>
+            ))}
           </tbody>
         </table>
       </div>
